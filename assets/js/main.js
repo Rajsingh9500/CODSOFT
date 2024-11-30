244c0d32c3d89761bdfd36712e457ba04f3a11a1
raj singh <114086748+Rajsingh9500@users.noreply.github.com>
2024-11-30 12:50:25 +0530

    by raj

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,13 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('hide');
 });
+
+
+function myFunction() {
+  var x = document.getElementById("card");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
