25c1d85643302b9b42e57320f97ce02551faade0
raj singh <114086748+Rajsingh9500@users.noreply.github.com>
2024-12-03 10:50:00 +0530

    by raj

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const hamburger = document.querySelector('.hamburger-menu');
+/* const hamburger = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener('click', () => {
@@ -13,4 +13,35 @@ function myFunction() {
   } else {
     x.style.display = "none";
   }
+} */
+/*    const hamburgerMenu = document.querySelector(".hamburger-menu");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburgerMenu.addEventListener("click", () => {
+      navMenu.classList.toggle("hide");
+  }); */
+  var count = 1 ;
+ if(count == 1)
+ {
+  function toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    navMenu.classList.toggle('show');
+
+    document.getElementById("down").style.marginTop = "220px";
+
 }
+ }else if(count == 0)
+ {
+
+
+    document.getElementById("down").style.marginTop = "10px";
+}
+
+
+ 
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      alert("Thank you for reaching out! We'll get back to you soon.");
+      form.reset();
+  });
